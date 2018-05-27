2465e0fb8c3ea17ee7ae9ae602a44391456d028e
PuppyRush <gooddaumi@naver.com>
2018-05-27 17:27:09 +0900

    item20

--- example/example/item20.cpp
+++ example/example/item20.cpp
@@ -21,10 +21,13 @@ void item20()
 	printer::print("ptr타입이 아니면 정상출력");
 	printer::printRange<int>(ary.begin(), ary.end());
 
+
 	const auto ptrcomp = [&](const int *lhs, const int *rhs){
 		return *lhs < *rhs;
 	};
 
+	printer::enter(2);
+	printer::print("pointer type을 위한 비교함수를 특별히 정의하여 set생성자에서 이용");
 	set<int*, decltype(ptrcomp)> ptr_compset(ptrcomp);
 	ptr_compset.insert(new int(34));
 	ptr_compset.insert(new int(32));
